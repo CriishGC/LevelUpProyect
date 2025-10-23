85b14b7a728c892e78616d8654345fb3eae21f60
PAAWSA02LC1321 <Duoc@PAAWSA02LC1321.duoc.acad>
2025-10-23 17:31:39 -0300

    Se hizo components casi comleto, falta cambiar algunas rutas y completar con lo que falte. Cristobal G vo sai

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+import UserForm from "../molecules/UserForm";
+
+const Home = () => (
+    <div>
+        <h1>Bienvenido a nuestra Tienda Online</h1>
+        <UserForm />
+        
+    </div>
+);
+
+export default Home;
