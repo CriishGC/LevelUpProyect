85cb83c37633e9b06517a5b7b2c6c637fe075d84
Jonathan Lillo <jon.lillo@duocuc.cl>
2025-10-30 17:54:23 -0300

    Terminación de configuración karma y jasmine, creación de jasmine.json

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,45 @@
-const WebpackConfig = require('./webpack.config.js');
+const webpackConfig = require('./webpack.config.js');
 
 module.exports = function(config) {
-  config.set({
-    frameworks: ['jasmine'],
-    files: [
-        '/src/**/*.test.js',
-        '/src/**/*.test.jsx',
-        '/src/**/*.test.ts',
-        '/src/**/*.test.tsx'
-    ],
-    preprocessors: {
-      './src/**/*.test.js': ['webpack'],
-      './src/**/*.test.jsx': ['webpack'],
-      './src/**/*.test.ts': ['webpack'],
-      './src/**/*.test.tsx': ['webpack']
-    },
-    webpack: WebpackConfig,
-    reporters: ['progress'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    concurrency: Infinity
-  });
-};
+    config.set({
+        framework: ['jasmine'],
+
+        files: [
+            'src/**/*.test.js',
+            'src/**/*.test.jsx',
+            'src/**/*.test.ts',
+            'src/**/*.test.tsx'
+        ],
+
+        preprocessors: {
+            'src/**/*.test.js' : ['webpack'],
+            'src/**/*.test.jsx' : ['webpack'],
+            'src/**/*.test.ts' : ['webpack'],
+            'src/**/*.test.tsx' : ['webpack'],
+        },
+
+        webpack: {
+            ...webpackConfig,
+            mode: 'development',
+            resolve: {
+                ...webpackConfig.resolve,
+                alias: {
+                    ...webpackConfig.resolve.alias,
+                }
+            }
+        },
+
+        browsers: ['Chrome'],
+        reporters: ['progress'],
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        singleRun: process.env.CI === 'true',
+        concurrency: Infinity,
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-webpack'
+        ]
+
+    })
+}
